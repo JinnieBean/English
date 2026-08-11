7d316a9e37e5ca0ff9ceb2119f48a99edfc5e73d
thoandbean <dangkhoado16@gmail.com>
2026-08-11 16:13:09 +0700

    fix picture on index

--- fix.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('assets/js/main.js', 'utf8');
-content = content.replace(/let (\w+)Html = ""; `\w+Html \+= `/g, '$1ListContainer.innerHTML += `');
-fs.writeFileSync('assets/js/main.js', content);
